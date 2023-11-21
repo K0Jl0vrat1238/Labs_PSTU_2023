2b299cbeaccb31ea6ea76b37a216cf99aa99ba22
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-11-21 21:19:11 +0500

    Add files via upload

--- Sem_1.gitkeep/1_3.gitkeep/1_3.cpp
+++ Sem_1.gitkeep/1_3.gitkeep/1_3.cpp
@@ -1,1 +1,19 @@
-
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float a;
+	cin >> a;
+	if (a < 5)
+	{
+		cout << a * 3;
+	}
+	else if (5 <= a <= 7)
+	{
+		cout << a / 10;
+	}
+	else
+	{
+		cout << a + 3;
+	}
+}
