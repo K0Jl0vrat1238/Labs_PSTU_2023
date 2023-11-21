9c4a03f8aaef94dc6a5e0f16be2e032eb444edc4
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-11-21 15:07:03 +0500

    Add files via upload

--- Sem_1.gitkeep/1_2.gitkeep/1_2.cpp
+++ Sem_1.gitkeep/1_2.gitkeep/1_2.cpp
@@ -1,1 +1,19 @@
+﻿#include <iostream>
+using namespace std;
 
+int main()
+{	
+	setlocale(LC_ALL, "RU");
+	float a, b;
+	cin >> a >> b;
+	if (b == 0)
+	{
+		cout << "не можем разделить";
+	}
+	else
+	{
+
+		cout << a / b;
+	}
+
+}
