e9b632d66fea2f592f7386c12cd964ae653b025c
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-11-21 21:34:21 +0500

    Add files via upload

--- Sem_1.gitkeep/2_1.gitkeep/2_1.cpp
+++ Sem_1.gitkeep/2_1.gitkeep/2_1.cpp
@@ -1,1 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int cnt;
+    int sum = 0;
+    cout << "Введите число: ";
+    cin >> cnt;
+    for (int count = 1; count <= cnt; count++)
+    {
+        
+        sum += count;
+
+    }
+    cout << sum;
+}
+
 
