0f7c9a51f8a97c6dfbecaff7a21b7f10e669e178
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-19 16:43:01 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_4/2_4_.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int spasec = n / 2;
+	int stars = 1;
+
+	for (int i = 0; i < (n + 1) / 2; i++)
+	{
+		for (int j = 0; j < spasec; j++)
+		{
+			cout << " ";
+		}
+		spasec--;
+		for (int j = 0; j < stars; j++)
+		{
+			cout << "*";
+		}
+		stars += 2;
+		cout << endl;
+	}
+	return 0;
+}
