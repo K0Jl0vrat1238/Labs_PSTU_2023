d051ff4d201fa3cdb5a91d26a36457f0a759785f
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-23 14:36:21 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_2/2_2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int cnt, b;
+    cin >> cnt;
+    int sum = 1;
+    for (int count = 1; count <= cnt; count += 1)
+    {
+        cin >> b;
+        sum *= b;
+    }
+    cout << sum;
+    return 0;
+}
