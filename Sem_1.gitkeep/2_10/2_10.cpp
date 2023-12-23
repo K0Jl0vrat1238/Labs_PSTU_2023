f18349742ae285ab6174bd4112cfcf735a2152a5
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-23 15:51:04 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_10/2_10.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    int n;
+    int s;
+    int max;
+    cout << "введиите кол-во чисел: ";
+    cin >> n;
+    if (n > 0)
+    {
+
+        cin >> max;
+        for (int i = 1; i < n; i++) 
+        {
+            cin >> s;
+            if (s >= max)
+            {
+                max = s;
+            }
+        }
+        cout << max;
+    }
+    else
+    {
+        cout << "Так не работает";
+    }
+    
+
+
+    return 0;
+}
