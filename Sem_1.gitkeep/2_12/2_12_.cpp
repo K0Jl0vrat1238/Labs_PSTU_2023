1253439b4544afee1c3f8f820cf9590c9d50d201
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:17:14 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_12/2_12_.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int cnt, n, max, min;
+
+    cout << "Введите количество чисел:";
+    cin >> cnt;
+
+    if(cnt != 0)
+    {
+        cin >> n;
+        max = min = n;
+
+        for (int i = 0; i < cnt-1; i++)
+        {
+            cin >> n;
+
+            if (n > max)
+            {
+                max = n;
+            }
+            if (n < min)
+            {
+                min = n;
+            }
+        }
+
+        cout << "Макс + Мин = " << max + min;
+
+    }
+    return 0;
+}
