98599f4fe8556405733cfd3170eabae8c001bbcf
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 22:42:11 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/equation/decison 2_.cpp
@@ -0,0 +1,21 @@
+﻿﻿//f = x - (1 / (3 + sin(3,6x)) 
+//f' = 1 + ((18*cos(3,6x)) / (5 * (3+sin(3,6x))**2)
+// f'' = (-3240 * sin(3,6x) - 1944 - 324 * sin(3,6x)*(cos(3,6x)**2)) / (25 * ((3+sin(3,6x))**4)
+////[0;0.85];
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	double x = 0, xperv = 0;
+	double eps = 0.000001;
+	while (abs(x - xperv) > eps)
+	{
+		xperv = x;
+		x = xperv - (xperv - (1 / (3 + sin(3.6 * xperv)))) / (1 + ((18 * cos(3.6 * xperv)) / (5 * pow((3 + sin(3.6 * xperv)), 2))));
+	}
+	cout << x;
+
+}
