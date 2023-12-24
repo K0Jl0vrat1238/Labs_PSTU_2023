045dd0dc00f7da1171c121169868b770a7fb359e
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:23:20 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_22/2_22.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int cnt = 0;
+	float n, s, m;
+
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++)
+	{
+		s = sin(n + i / n);
+
+		if (i == 0)
+		{
+			m = s;
+			cnt = i;
+		}
+
+		if (s > m)
+		{
+			m = s;
+			cnt = i;
+		}
+
+	}
+
+	cout << m << endl << cnt;
+
+
+}
