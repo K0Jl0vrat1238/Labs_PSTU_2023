33887eb60571aafdd2064aa0da8f78edf3127b17
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:54:04 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/pointer/Указатели.cpp
@@ -0,0 +1,114 @@
+﻿
+#include <iostream>
+using namespace std;
+#include <string>
+int main()
+{
+	//1
+	//int a, b, time;
+	//int* ptr1;
+	//int* ptr2;
+	//cin >> a >> b;
+	//ptr1 = &a;
+	//ptr2 = &b;
+	//time = *ptr1;
+	//*ptr1 = *ptr2;
+	//*ptr2 = time;
+	//cout << *ptr1 <<endl<< *ptr2;
+
+	//2
+	//int a, b, * ptr1, * ptr2;
+	//cin >> a >> b;
+	//ptr1 = &a;
+	//ptr2 = &b;
+	//cout << *ptr1 + *ptr2;
+
+	//3
+	//float a, b, * ptr1, * ptr2;
+	//cin >> a >> b;
+	//ptr1 = &a;
+	//ptr2 = &b;
+	//cout << *ptr1 + *ptr2 << endl;
+
+	//4
+	//int n, tmp, * ptr = &tmp, max, min;
+	//cin >> n;
+	//cin >> *ptr;
+	//max = *ptr;
+	//min = *ptr;
+	//for (int i = 1; i < n; i++)
+	//{
+	//	cin >> *ptr;
+	//	if (*ptr > max) { max = *ptr; }
+	//	else if (*ptr < min) { min = *ptr; }
+	//}
+	//cout <<  max << endl;
+	//cout  << min << endl;
+
+	//5
+	//int n, f = 1;
+	//int* ptr = &f;
+	//cin >> n;
+	//for (int i = 1; i <= n; i++)
+	//{
+	//	*ptr *= i;
+	//}
+	//cout << f;
+	
+	//6
+	//setlocale(LC_ALL, "ru_RU");
+	//int gl = 0, sgl = 0;
+	//string st;
+	//cin >> st;
+	//const char* ptr = st.c_str();
+	//for (int i = 0; i < st.length(); i++)
+	//{
+	//	if (*ptr == 'A' or *ptr == 'E' or *ptr == 'I' or *ptr == 'O'
+	//		or *ptr == 'Y' or *ptr == 'U' or *ptr == 'a' or *ptr == 'e'
+	//		or *ptr == 'i' or *ptr == 'y' or *ptr == 'o' or *ptr == 'u')
+	//	{
+	//		gl++;
+	//	}
+	//	else
+	//	{
+	//		sgl++;
+	//	}
+	//	ptr++;
+	//}
+	//cout << gl<< endl<<sgl;
+	
+	//7
+	//int n;
+	//int* ptr = &n;
+	//cin >> *ptr;
+	//cout << n;
+	
+	//8
+	//string st, a1, a2;
+	//cin >> st >> a1 >> a2;
+	//int a_1 = 0, a_2 = 0, other = 0;
+	//for (int i = 0; i < st.length(); i++)
+	//{
+	//	string s = st.substr(i, 1);
+	//	if (s == a1)
+	//	{
+	//		a_1++;
+	//	}
+	//	else if (s = a2)
+	//	{
+	//		a_2++;
+	//	}
+	//	else
+	//	{
+	//		other++;
+	//	}
+	//	cout << a_1 << endl << a_2 << endl << other;
+	//}
+		
+
+
+
+}
+
+
+
