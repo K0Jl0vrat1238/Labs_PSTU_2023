38d661ebb27ff02eed9b61afecf6a00b99b2f0e3
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:22:27 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_20/2_20.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float n, s, b;
+	int i = 1;
+	bool f = false;
+	cin >> n >> s;
+
+	for (int i = 0; i < n; i++)
+	{
+		b = sin(n + i / n);
+		b = round(b * 100) / 100;
+		cout << b<< endl;
+
+		if (b == s)
+		{
+			f == true;
+		}
+	}
+
+	if (f)
+	{
+		cout << "found";
+	}
+	else
+	{
+		cout << "no found";
+	}
+}
