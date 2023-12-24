1155d73526c7797bdbc5961c9716cf27d2b098b7
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:19:04 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_14/2_14.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum = 0;
+    cin >> n;
+    
+
+    while (n > 0)
+    {
+        int number = n % 10;
+        sum += number;
+        n /= 10;
+
+    }
+    cout << sum;
+    return 0;
+}
+
