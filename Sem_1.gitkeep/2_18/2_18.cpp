c63ea9d2aed78818f0623c4568ab0160bac42a52
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:21:40 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_18/2_18.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int rev = 0;
+    cin >> n;
+
+
+    while (n > 0)
+    {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+
+    }
+    cout << rev;
+    return 0;
+}
+
