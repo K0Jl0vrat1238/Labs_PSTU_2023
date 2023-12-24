61523533a1d067931fced3f21c4217dab50f8cac
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:20:00 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_15/2_15.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int number_s;
+	bool f = true;
+	cin >> n>> number_s;
+	
+	while ( (n > 0) and (f = true))
+	{
+		int degit_number = n % 10;
+		if (degit_number == number_s)
+		{
+			cout << "the figure is in this number";
+			f = false;
+		}
+		n /= 10;
+	}
+	return 0;
+}
+
