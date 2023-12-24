af3ae310e22f62f15bc1b735334c1c556965e853
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:22:06 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_19/2_19.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int sum = 1;
+	cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		if ((i % 3) != 0)
+		{
+			sum += i;
+		}
+		else
+		{
+			sum -= i;
+		}
+	}
+	cout << sum;
+}
