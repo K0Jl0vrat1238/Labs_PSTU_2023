9f1664dbc380d4fed1074d53f7aea63eabaa89ce
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:18:19 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_13/2_13.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "a:";
+	cin >> a;
+	cout << "b:";
+	cin >> b;
+	cout << "c:";
+	cin >> c;
+	int discriminant = b * b + 4 * a * c;
+	if (discriminant > 0)
+	{
+		int x1 = (-b + sqrt(discriminant)) / (2 * a);
+		int x2 = (-b - sqrt(discriminant)) / (2 * a);
+		cout << x1 << x2;
+	}
+	else if (discriminant == 0)
+	{
+		int x = (-b) / (a * 2);
+		cout << x;
+	}
+	else
+	{
+		cout << "there is no solution";
+	}
+	return 0;
+}
