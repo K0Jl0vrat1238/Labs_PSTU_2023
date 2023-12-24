e32c59a3a6a3e9d92a5caaca56f913d48b0a2da0
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 22:01:50 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/equation/Decision 1.cpp
@@ -0,0 +1,35 @@
+﻿//x - (1 / (3 + sin(3,6x)) = 0
+//[0,0.85]
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float a = 0, b = 0.85, c;
+	float fA, fB, fC;
+	double eps = 0.000001;
+	while (b - a > eps)
+	{
+		c = ((a + b) / 2);
+		fA = a - (1 / (3 + sin(3.6 * a)));
+		fB = b - (1 / (3 + sin(3.6 * b)));
+		fC = c - (1 / (3 + sin(3.6 * c)));
+		
+		if (fA * fC < 0)
+		{
+			a = c;
+		}
+		else if (fB * fC < 0)
+		{
+			b = c;
+		}
+		else
+		{
+			cout << "wrong";
+		}
+		
+	}
+	cout << a << endl << b;
+	return 0;
+}
