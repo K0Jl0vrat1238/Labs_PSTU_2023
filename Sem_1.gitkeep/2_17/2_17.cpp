dd95997b44bcd9db1d58fe98c8cb2859c05b96bf
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:21:16 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_17/2_17.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int n, x, r;
+	double  sum;
+	sum = r = 1;
+
+	cin >> n >> x;
+
+	for (int i = 1; i <=n; i++)
+	{
+		r *= i;
+		sum += (pow(x, i )/ r);
+	}
+	cout << sum;
+
+	return 0;
+
+}
