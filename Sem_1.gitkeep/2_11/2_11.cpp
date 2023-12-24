dd87eaf797e08f3d2bdf3c11f4fc3128af5401e5
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:15:31 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_11/2_11.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+    setlocale(LC_ALL, "RU");
+
+    int n, cnt;
+    bool pos = false;
+    bool neg = false;
+
+    cout << "Кол-во чисел: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) 
+    {
+;
+        cin >> cnt;
+
+        if (neg == false and pos == false) 
+        {
+            if (cnt > 0) 
+            {
+                pos = true;
+            }
+            else if (cnt < 0) 
+            {
+                neg = true;
+            }
+        }
+    }
+
+    if (pos == true) 
+    {
+        cout << "Сначало положительное";
+    }
+    else if (neg == true) 
+    {
+        cout << "Сначало отрицательное";
+    }
+    else 
+    {
+        cout << "Одни нули";
+    }
+    return 0;
+}
