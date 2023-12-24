4ad7296dcf398c54d0cf8381ff763fd3a6f82edb
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2023-12-24 17:22:48 +0500

    Add files via upload

---
+++ Sem_1.gitkeep/2_21/2_21.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, t;
+	bool f = true;
+
+	cin >> t;
+
+	while (t != 0)
+	{
+		cin >> n;
+		if ((n != 0) and (t > n))
+		{
+			f = false;
+		}
+		t = n;
+	}
+	if (f)
+	{
+		cout << "Ascending order";
+	}
+	else
+	{
+		cout << "not ascending";
+	}
+	return 0;
+}
