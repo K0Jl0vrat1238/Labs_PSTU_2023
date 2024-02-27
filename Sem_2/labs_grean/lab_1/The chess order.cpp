2600aa49ec3f2d9f056e00c0abd24b9c9204d45c
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2024-02-27 11:35:58 +0500

    Add files via upload

---
+++ Sem_2/labs_grean/lab_1/The chess order.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 9, m = 9;
+	int a[n][m];
+	int c = 1;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if ((i + j) % 2 == 1)
+			{
+				a[i][j] = 0; 
+			}
+			else
+			{
+				if (c == 10)
+				{
+					c = 1;
+				}
+				a[i][j] = c;
+				c++;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << a[i][j] << "\t";
+
+		}
+		cout << endl;
+	}
+}
