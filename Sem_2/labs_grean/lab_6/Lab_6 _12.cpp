8dd151f3c96d3fb2d12b89ac444567b606a79ae8
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2024-02-27 11:55:32 +0500

    Add files via upload

---
+++ Sem_2/labs_grean/lab_6/Lab_6 _12.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// Функция, которая разворачивает слово
+void reverse_word(string& word) 
+{
+    int left = 0; 
+    int right = word.size() - 1; 
+    while (left < right) 
+    { 
+        swap(word[left], word[right]); 
+        left++; 
+        right--; 
+    }
+}
+
+// Функция, которая преобразует строку
+string trans_string(const string& s) 
+{
+    string result;
+    string word; 
+    for (char c : s) 
+    { 
+        if (c == ' ') 
+        { 
+            reverse_word(word); 
+            result += word + " "; 
+            word = ""; 
+        }
+        else 
+        { 
+            word += c; 
+        }
+    }
+    reverse_word(word); 
+    result += word; 
+    return result; 
+}
+
+int main() 
+{
+    
+    string s = "Mom and dad are parents";
+    
+    string t = trans_string(s);
+    
+    cout << t << endl;
+
+    return 0;
+}
