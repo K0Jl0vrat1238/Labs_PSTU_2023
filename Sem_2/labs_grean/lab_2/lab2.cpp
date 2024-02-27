77e797218dfa35476faf5ad01043bb8bc8490952
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2024-02-27 11:42:05 +0500

    Add files via upload

---
+++ Sem_2/labs_grean/lab_2/lab2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+const int N = 100; 
+
+int main() {
+    int matrix[N][N];
+    int col;
+    cin >> col;
+    cout << "\n";
+
+    
+    for (int i = 0; i < col; ++i)
+    {
+        for (int j = 0; j < col; ++j) 
+        {
+            if (i >= j) 
+            {
+                matrix[i][j] = (i-j+1)%10; 
+            }
+            else if (i < j) 
+            {
+                matrix[i][j] = 0; 
+            }
+            else if(i-j>=9)
+            {
+                matrix[i][j] = (i - j+1)%10+1; 
+                
+            }
+           
+        }
+        
+    }
+
+    
+
+    for (int i = 0; i < col; ++i) 
+    {
+        for (int j = 0; j < col; ++j) 
+        {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << std::endl;
+    }
+
+    return 0;
+}
