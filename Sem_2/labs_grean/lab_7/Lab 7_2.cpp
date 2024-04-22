e89210772476a46dfa044aa1b7fea2edd9a20d2c
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2024-04-22 14:17:20 +0500

    Add files via upload

---
+++ Sem_2/labs_grean/lab_7/Lab 7_2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int max(int m, ...)
+{
+	int* p = &m + 2;
+	int mx = *p;
+	for (int i = 0;i<m; i++)
+	{
+		if (mx < *p)
+		{
+			mx = *p;
+		}
+		p+=2;
+	}
+	return mx;
+}
+double max(double m, ...)
+{
+	double* p = &m + 2;
+	double mx = *p;
+	for (int i = 0; i < m; i++)
+	{
+		if (mx < *p)
+		{
+			mx = *p;
+		}
+		p += 2;
+	}
+	return mx;
+}
+
+int main()
+{
+	cout << max(5, 6, 9, 4, 7, 8)<<"\n";
+	cout << max(10.0, 3.432, 3.657, 6.3424, 5.432, 7546.4 , 4353.4, 345.543, 4.435, 34.45, 32.0)<<"\n";
+	cout << max(12, 54, 546, 23, 5, 8, 0, 3, -6, 6, 3, 6, 7)<<"\n";
+}
---
+++ Sem_2/labs_grean/lab_7/l_7_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+double sum(double a, double b)
+{
+	double s = a + b;
+	return s;
+}
+int sum(int a, int b, int az, int bz, int& com_div, int& s)
+{
+	com_div = az * bz;
+	a = a * bz;
+	b = b * az;
+	s = a + b;
+	return (com_div, s);
+}
+int main() 
+{
+	double da, db;
+	cout << "the first decimal fraction" << "\n";
+	cin >> da;
+	cout << "the second decimal fraction" << "\n";
+	cin >> db;
+	double k = sum(da, db);
+	cout << "The sum of decimals " << k<<"\n";
+
+	int ia, ib, iaz, ibz, denominator, numerator;
+	cout << "Enter the numerator and denominator of the first fraction" << "\n";
+	cin >> ia >> iaz;
+	cout << "Enter the numerator and denominator of the second fraction" << "\n";
+	cin >> ib >> ibz;
+	sum(ia, ib, iaz, ibz, denominator, numerator);
+	
+	cout << "The sum of ordinary fractions " << numerator << " / " << denominator;
+	return 0;
+
+}
