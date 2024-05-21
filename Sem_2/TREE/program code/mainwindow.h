99824e8b6fdc99a1cfee95aac536e47cb8750377
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2024-05-21 12:57:28 +0500

    Add files via upload

---
+++ Sem_2/TREE/program code/mainwindow.h
@@ -0,0 +1,33 @@
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include "tree.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    Tree tree;
+    QGraphicsScene *scene;
+    QGraphicsView *graphicsView;
+private slots:
+    void Forming_tree();
+    void Print_tree(tnode* root);
+    void Print_Binary();
+    void Print_MAX();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
