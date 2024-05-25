1142d785f3fa86b57f5a6fbcaf7684aed6204b96
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2024-05-25 13:57:18 +0500

    Add files via upload

---
+++ Sem_2/labs_grean/l_13/L13_shelSort.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+using namespace std;
+void shellSort(int* arr, int n) {
+    // Начинаем с большого интервала, затем уменьшаем его
+    for (int interval = n / 2; interval > 0; interval /= 2) {
+        // Делаем сортировку вставками для этого интервала
+        for (int i = interval; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= interval && arr[j - interval] > temp; j -= interval) {
+                arr[j] = arr[j - interval];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+// Пример использования функции shellSort
+int main() {
+    system("chcp 1251 >Null");
+    int arr[] = { 12, 34, 54, 2, 3 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Исходный массив: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+
+    shellSort(arr, n);
+
+    cout << "Отсортированный массив: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+
+    return 0;
+}
