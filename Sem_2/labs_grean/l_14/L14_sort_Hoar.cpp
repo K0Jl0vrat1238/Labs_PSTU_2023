5662bbf9aa57a23114b412c2b111c2726f5a342d
K0Jl0vrat1238 <146915138+K0Jl0vrat1238@users.noreply.github.com>
2024-06-05 18:05:26 +0500

    Add files via upload

---
+++ Sem_2/labs_grean/l_14/L14_sort_Hoar.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] > pivot) {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        int partitionIndex = i + 1;
+
+        quickSort(arr, low, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = { 12, 4, 5, 6, 7, 3, 1, 15 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array (descending order): ";
+    printArray(arr, n);
+
+    return 0;
+}
